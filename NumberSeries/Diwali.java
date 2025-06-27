b78404463aba95a88f896e7821abd0ee7299832d
kurmirajendra <rajendrakurmi78@gmail.com>
2025-06-28 00:30:43 +0530

    Add files via upload

---
+++ NumberSeries/Diwali.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Veersingh Lodhi
+ */
+import java.awt.*;
+import java.sql.*;
+import javax.swing.*;
+public class Diwali extends JFrame{
+    
+    Diwali()
+    {
+        
+    }
+}
