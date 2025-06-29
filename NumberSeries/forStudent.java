dc885f4790212d648d4ca5dbf22f5a6ad27184dc
kurmirajendra <rajendrakurmi78@gmail.com>
2025-06-29 19:15:04 +0530

    Add files via upload

---
+++ NumberSeries/forStudent.java
@@ -0,0 +1,25 @@
+
+public class forStudent {
+
+    
+    public static void main(String[] args) {
+        
+        School_Student s1=new School_Student();
+        s1.setStudent(101, "Veer Singh Lodhi", "Male");
+        s1.setSchool_Student("10th");
+        s1.getStudent();
+        s1.getSchool_Student();
+        
+        System.out.println("================================================");
+        
+        CollegeStudent c1=new CollegeStudent();
+        c1.setStudent(102, "Krishna Kurmi", "Male");
+        c1.setCollegeStudent("CS");
+        c1.getStudent();
+        c1.getCollegeStudent();
+        
+        
+        
+    }
+    
+}
