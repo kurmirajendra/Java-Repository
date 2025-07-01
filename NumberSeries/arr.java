cf083d146d83dccf102f12924cad7a5d525aaa54
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-01 21:39:20 +0530

    Update arr.java

--- NumberSeries/arr.java
+++ NumberSeries/arr.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Veersingh Lodhi
- */
 class arr {
+    int arr[]={1,2,3,4,5};
+
+    System.out.println(arr[2]);
     
 }
