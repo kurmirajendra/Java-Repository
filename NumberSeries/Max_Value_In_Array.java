d58a19e44c72390c07faa8869947a7903b486554
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-02 22:18:46 +0530

    Add files via upload

---
+++ NumberSeries/Max_Value_In_Array.java
@@ -0,0 +1,9 @@
+
+package firstproject;
+
+public class Max_Value_In_Array {
+       public static void main(String [] args)
+       {
+           System.out.println("Veer Singh Lodhi");
+       }
+}
