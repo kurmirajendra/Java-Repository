961ecbb4271fd8b420ce7feceaf06f76da74a25a
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-04 21:38:21 +0530

    Add files via upload

--- NumberSeries/Student.java
+++ NumberSeries/Student.java
@@ -1,19 +1,33 @@
 
-public class Student {
-        
-    int rollno;
-    String name,gender;
-    void setStudent(int rollno,String name,String gender)
+public class Student extends SchoolMember {
+    int gradeLevel;
+    double gpa;
+    Student(String name,String id,int age,int gradeLevel,double gpa)
     {
-        this.rollno=rollno;
-        this.name=name;
-        this.gender=gender;
+        super(name,id,age);
+        this.gradeLevel=gradeLevel;
+        this.gpa=gpa;
+    }
+    @Override
+    public void displayDetails()
+    {
+        super.displayDetails();
+        System.out.println("I am in "+gradeLevel+" grade ");
+        System.out.println(" GPA "+gpa);
+    }
+    @Override
+    String introduce()
+    {
+        return super.introduce()+" I am in grade "+gradeLevel+"with GPA "+gpa;
+    }
+    @Override
+    String getRole()
+    {
+        return " Student ";
+    }
+    public void study()
+    {
+        System.out.println(name+"very good in study ");
     }
     
-    void getStudent()
-    {
-        System.out.println("Roll no is "+rollno);
-        System.out.println("Name is "+name);
-        System.out.println("Gender "+gender);
-    }
 }
