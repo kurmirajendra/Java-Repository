9893cc0e04095715ad4a67cab10af48f86afccc1
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-05 17:18:37 +0530

    Add files via upload

---
+++ NumberSeries/Functions.java
@@ -0,0 +1,12 @@
+
+public class Functions {
+
+    public static void main(String[] args) {
+        String str="Lets Learn java ";
+        int len = str.length();
+        System.out.println("Length "+len);
+        System.out.println("Hello".length());
+    }
+    
+    
+}
