711e54d10910b80ca0afc56cba910d507f791342
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-16 21:16:44 +0530

    Add files via upload

---
+++ JavaPractice/BankAbbreviation.java
@@ -0,0 +1,14 @@
+
+public class BankAbbreviation {
+
+   
+    public static void main(String[] args) {
+        String str="State Bank Of India";
+         char ch1=str.charAt(0);
+         char ch2=str.charAt(str.indexOf("Bank"));
+         char ch3=str.charAt(str.indexOf("India"));
+         String abbr=""+ch1+ch2+ch3;
+         System.out.println("Abbreviation "+abbr);
+    }
+    
+}
