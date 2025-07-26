682d81556bac3efac0d4d69aaee231e7cd436f42
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-26 22:06:51 +0530

    Update BankAbbreviation.java

--- JavaPractice/BankAbbreviation.java
+++ JavaPractice/BankAbbreviation.java
@@ -3,7 +3,7 @@ public class BankAbbreviation {
 
    
     public static void main(String[] args) {
-        String str="State Bank Of India";
+        String str=" State Bank Of India ";
          char ch1=str.charAt(0);
          char ch2=str.charAt(str.indexOf("Bank"));
          char ch3=str.charAt(str.indexOf("India"));
