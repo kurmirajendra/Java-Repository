efc0439050421c45b75318f2e8f7552b1a6714e3
kurmirajendra <rajendrakurmi78@gmail.com>
2025-07-29 21:28:26 +0530

    Add files via upload

---
+++ JavaPractice/Main2nd.java
@@ -0,0 +1,21 @@
+public class Main2nd {
+
+    public static void main(String[] args) {
+         int num =10;
+         boolean isPrime = true;
+         for(int i=2;i<=num/2;i++)
+         {
+             if(num%i==0){
+                 isPrime=false;
+                 break;
+             }
+         }
+          if(isPrime)
+        System.out.println(num+" is a prime number ");
+          else 
+              System.out.println(num+" is not a prime number ");
+    }
+   
+    
+    
+}
