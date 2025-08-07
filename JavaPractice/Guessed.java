3f29c8c694c2891d008374493b780ed5b869b52d
kurmirajendra <rajendrakurmi78@gmail.com>
2025-08-07 22:13:34 +0530

    Add files via upload

---
+++ JavaPractice/Guessed.java
@@ -0,0 +1,26 @@
+
+public class Guessed {
+
+   
+    public static void main(String[] args) {
+        int []numbers={2,3,5,6,8,9,1,7,1};
+        System.out.println("Numbers in array ");
+        for(int i=0;i<numbers.length;i++)
+        {
+            System.out.println(numbers[i]+" ");
+        }
+        System.out.println("\n");
+        int guessedIndex=5;
+        System.out.println("To access the number at index: "+guessedIndex);
+        try
+        {
+            int guessedNumber=numbers[guessedIndex];
+            System.out.println("Guessed number: "+guessedNumber);
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Error:index "+guessedIndex+ " is out of bounds. Valid index are 0 to "+(numbers.length-1));
+        }
+    }
+    
+}
