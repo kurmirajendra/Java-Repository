1ba2d138854f6f04fe10b8436af21daaaefcb0e1
kurmirajendra <rajendrakurmi78@gmail.com>
2025-09-03 21:58:32 +0530

    Add files via upload

---
+++ NumberSeries/Square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Square {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter any number: ");
+        int n = sc.nextInt();
+        System.out.println("Square of "+n+" = "+(n*n));
+    }
+    
+}
