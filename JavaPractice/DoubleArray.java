a69f589764b27da8616c12ee2c69534f4fa07648
kurmirajendra <rajendrakurmi78@gmail.com>
2025-09-07 21:24:16 +0530

    Add files via upload

---
+++ JavaPractice/DoubleArray.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author RAJENDRA
+ */
+public class DoubleArray {
+
+    public static void main(String[] args) {
+        int [][] arr ={{78,78},{90,89}};
+        System.out.println("Array: "+arr[0][1]);
+        System.out.println("2nd Array: "+arr[1][0]);
+    }
+    
+}
